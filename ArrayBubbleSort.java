5c0fb4ea07fc1de0c8ffd05762ea93bfc4c4a4f4
Rohit <rohitmonu154@gmail.com>
2023-09-03 21:02:34 +0530

    Bubble Sorting

--- ArrayBubbleSort.java
+++ ArrayBubbleSort.java
@@ -14,12 +14,12 @@ public class ArrayBubbleSort {
         for (int i = 0; i < arr.length; i++) {
             check = false;
             for (int j = 1; j < arr.length - i; j++){
-             if (arr[j-1] > arr[j]){
+                if (arr[j-1] > arr[j]){
                      int temp = arr[j];
                      arr[j] = arr[j - 1];
                      arr[j - 1] = temp;
                      check = true;
-             }
+                }
             }
         if (!check)
             break;
