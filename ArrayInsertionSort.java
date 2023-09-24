c9a37aeeb5ad8b6fa9c52d9cfad2637f263fb45c
Rohit <rohitmonu154@gmail.com>
2023-09-24 09:39:47 +0530

    Insertion Sort

--- ArrayInsertionSort.java
+++ ArrayInsertionSort.java
@@ -1,2 +1,20 @@
-package com.Javadsa;public class ArrayInsertionSort {
+package com.Javadsa;
+
+import java.util.Arrays;
+
+public class ArrayInsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 4, 1, 2};
+        for (int i = 0; i <= arr.length - 2; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if (arr[j-1] > arr[j]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                } else
+                    break;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
