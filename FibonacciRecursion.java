1f60634dc21b7d91ddfb4540501ea991ec1d8daf
Rohit <rohitmonu154@gmail.com>
2023-09-25 16:36:46 +0530

    Recursion

--- FibonacciRecursion.java
+++ FibonacciRecursion.java
@@ -1,2 +1,19 @@
-package com.Javadsa;public class FibonacciRecursion {
+package com.Javadsa;
+
+import java.util.Scanner;
+
+public class FibonacciRecursion {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter Term : ");
+        int term = in.nextInt();
+        System.out.println(term + " term of Fibonnaci Series is "+fib(term));
+    }
+    static int fib(int term){
+        if (term == 1 || term == 2)
+            return 1;
+        else
+            return fib(term-1) + fib(term - 2);
+    }
 }
--- GCDRecursion.java
+++ GCDRecursion.java
@@ -1,2 +1,20 @@
-package com.Javadsa;public class GCDRecursion {
+package com.Javadsa;
+
+public class GCDRecursion {
+    public static void main(String[] args) {
+
+        System.out.println(GCD(4,6));
+    }
+    static int GCD(int num1, int num2){
+        if (num1 == num2)
+            return num1;
+        if (num1 % num2 == 0)
+            return num2;
+        if (num2 % num1 == 0)
+            return num1;
+        if (num1 > num2)
+            return GCD(num1%num2,num2);
+        else
+            return GCD(num1, num2%num1);
+    }
 }
